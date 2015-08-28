f8a56e62d77bf2586d357de6c5369a0380026082
Marcus Albertsson <marcus.arubertoson@gmail.com>
2015-08-28 11:40:36 +0200

    updated docstrings

--- mdynaimport.py
+++ mdynaimport.py
@@ -1,36 +1,13 @@
 # -*- coding: utf-8 -*-
+
 """
     mdynaimport.py
     ~~~~~~~~~~~~~~
 
-    Was tired of constantly having to check my paths, so created a script
-    that places search paths in maya specific environments.
-
-    Place mdynaimport and userSetup.py in your script rootfolder, for
-    convenience I use:
-
-        "c:\users\<username>\Documents\maya\script"
-
-    which should be on the same level as the normal preference (2015-x64/2015)
-    folder. If you already have a userSetup.py just add the lines:
-
-        import mdynaimport
-        mdynaimport.parse_paths()
-
-    I've structured my folder to contain one MEL and one PYTHON directory.
-
-        script/
-            MEL/
-            PYTHON/
-            userSetup.py
-            mdynaimport.py
-
-    That allows me to just drag and drop scripts/script-dir straight into
-    the folders without additional setup.
-
-    If you spread out your scripts over several folders just have a
-    userSetup.py in each and add the above lines. The mdynaimport.py only
-    needs to exist in one of the folders for you to have access to it.
+    Was tired of not being able to create a custom directory structure
+    without having to constantly fiddle with paths. With mdynaimport in
+    a scriptdirectory maya can see just put a userSetup.py in the topmost
+    directory and add the below lines.
 
     USAGE::
 
@@ -46,7 +23,7 @@ import collections
 __title__ = 'mdynaimport'
 __author__ = 'Marcus Albertsson'
 __email__ = 'marcus.arubertoson@gmail.com'
-__url__ = "PATH/TO/GIT"
+__url__ = 'http://github.com/arubertoson/maya-mdynaimport'
 __version__ = '0.1.0'
 __license__ = 'MIT'
 __description__ = 'Dynamically import script paths to maya.'
